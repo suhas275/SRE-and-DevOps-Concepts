1038e1bf9762470342797af86546bf8654542437
suhas <suhasgowdam275@gmail.com>
2025-03-28 20:03:47 +0530

    modification

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,12 +4,12 @@ provider "aws" {
     region = "ap-south-1"
 }
 
-# Resources Block
-resource "aws_instance" "example_server" {
-    ami           = "ami-05c179eced2eb9b5b"
-    instance_type = "t2.micro"
+# # Resources Block
+# resource "aws_instance" "example_server" {
+#     ami           = "ami-05c179eced2eb9b5b"
+#     instance_type = "t2.micro"
 
-    tags = {
-        Name = "MyTerraformInstance"
-    }
-}
+#     tags = {
+#         Name = "MyTerraformInstance"
+#     }
+# }
